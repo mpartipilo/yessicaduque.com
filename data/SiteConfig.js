658e75e2aecb8d96120d330ed4f74acac71bbac0
Michelangelo Partipilo <michelangelo@zirconia.local>
2017-11-11 10:25:39 +0100

    Added Google Analytics ID

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -7,14 +7,14 @@ module.exports = {
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "Professional website from Yessica Duque about food styling, cooking and professional photography.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
+  siteFBAppID: "", // FB Application ID for using app insights
+  googleAnalyticsID: "UA-109211942-1", // GA tracking ID.
   disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
   postDefaultCategoryID: "Recipes", // Default category for posts.
   userName: "yessica", // Username to display in the author segment.
   userTwitter: "YekaMagenta", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "The Hague, Netherlands", // User location to display in the author segment.
-  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userAvatar: "http://yessicaduque.com/img/profile.jpg", // User avatar to display in the author segment.
   userDescription:
     "Professional food stylist and photographer", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
