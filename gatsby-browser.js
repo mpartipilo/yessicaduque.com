407738bc8b139925eb1cea2a4ec8ed3b1e274f05
Michelangelo Partipilo <michelangelo.partipilo@gmail.com>
2018-11-23 23:30:04 +0100

    Fix scroll bug

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,10 @@ exports.onClientEntry = () => {
   // Don't need to do anything here, but if you don't
   // export something, the import won't work.
 };
+
+const transitionDelay = 250;
+
+exports.shouldUpdateScroll = () => false;
+
+exports.onRouteUpdate = () =>
+  window.setTimeout(() => window.scrollTo(0, 0), transitionDelay);
